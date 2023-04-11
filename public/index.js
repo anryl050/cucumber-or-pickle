e15ee4263aad44a5da765199dd306d22e87adf3f
J Maltby <jacksonmaltby@gmail.com>
2023-04-10 20:29:25 -0700

    Added function in public/index.js for creating new chart with poll.

--- public/index.js
+++ public/index.js
@@ -26,3 +26,34 @@ async function voteOnPoll(choice) {
         console.error(err);
     }
 }
+
+async function createNewPoll() {
+    try {
+        const opinion = document.querySelector('#opinion-input').value; //needs to attach to the form HTML element
+        const response = await fetch('api/polls', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ opinion })
+        });
+        const result = await response.json();
+        console.log(result);
+
+        const newPollHtml = `
+            <div class="poll" id="${result.id}">
+                <h2>${question}</h2>
+                <div class="chart-container">
+                    <canvas id="${result.id}-chart"></canvas>
+                </div>
+                <button id="agree-button">Agree</button>
+                <button id="disagree-button">Disagree</button>
+            </div>
+        `;
+        document.querySelector('#poll-container').insertAdjacentHTML('beforeend', newPollHtml);
+
+        displayPollResults(result.id, `${result.id}-chart`);
+    } catch (err) {
+        console.error(err);
+    }
+}
