4ac9328edeab6f83ff72b0ef554f9ff89ab87878
J Maltby <jacksonmaltby@gmail.com>
2023-04-10 19:37:17 -0700

    Continued implementation of chart.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./home-routes.js');
+const apiRoutes = require("./api-routes.js");
 
 router.use('/', homeRoutes);
+router.use("/api", apiRoutes)
 
 module.exports = router;
