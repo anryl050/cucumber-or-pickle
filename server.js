ba516145d02f8fa1869bcbdb38932a8f29a68b95
whitneywishart <whitneywishart@gmail.com>
2023-04-10 21:51:16 -0700

    correct seeding errors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 const routes = require('./controllers');
-// const sequelize = require('./config/connection');
+// const sequelize = require('./config/connections');
 const helpers = require('./utils/helpers');
 
 const app = express();
