cd53f798131d500f6b9f11598a7faeee1b519f17
anryl050 <118693401+anryl050@users.noreply.github.com>
2023-04-11 21:11:18 -0700

    minor updates throughout the code

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,6 @@
 module.exports = {
   format_date: date => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-      date
-    ).getFullYear()}`;
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
   },
   format_plural: (word, amount) => {
       if (amount !== 1) {
