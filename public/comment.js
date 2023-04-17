9226405913ffbbb8d23030eb182f7a4dddb25cf9
whitneywishart <whitneywishart@gmail.com>
2023-04-17 12:33:58 -0700

    add functional comments

--- public/comment.js
+++ public/comment.js
@@ -1,70 +1,29 @@
-const comment_text = document.querySelector('#comment-text').value.trim();
-    const poll_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+async function commentFormHandler(event) {
+  event.preventDefault();
 
-const commentFormHandler = async (event) => {
+  const comment_text = document.querySelector("#comment-text").value;
+  const poll_id = document.querySelector("#poll-id").value;
 
-  if (event.target.hasAttribute('data-id')){
-    const id = event.target.getAttribute('data-id');
 
-    if (comment_text && poll_id) {
-      const response = await fetch(`/api/polls/${id}`, {
-        method: 'POST',
-        body: JSON.stringify({
-          comment_text,
-          poll_id
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }        
-      });} 
+  if (comment_text) {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        poll_id,
+        comment_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
     if (response.ok) {
-    document.location.reload();
-  } else {
-  alert('Sorry, comment submission failed. Please try again in a moment.');
-}}
-};
+      document.location.reload();
+      console.log(comment_text);
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
 
-console.log('button is clicked')
 document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
-
-
-
-// async function commentFormHandler(event) {
-//     event.preventDefault();
-  
-//     const comment_text = document.querySelector('#comment-text').value.trim();
-//     const poll_id = window.location.toString().split('/')[
-//       window.location.toString().split('/').length - 1
-//     ];
-  
-//     if (comment_text && poll_id) {
-//       const response = await fetch('/api/comments', {
-//         method: 'POST',
-//         body: JSON.stringify({
-//           comment_text,
-//           poll_id
-//         }),
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-
-        
-//       });
-//       console.log(comment_text && poll_id);
-  
-//       // if (response.ok) {
-//       //   document.location.reload();
-//       // } else {
-//       //   alert(response.statusText);
-//       // }
-//       if (response.ok) {
-//            document.location.reload();
-//     } else {
-//         alert('Sorry, comment submission failed. Please try again in a moment.');
-//     }
-//     }
-//   }
-  
-//   document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
